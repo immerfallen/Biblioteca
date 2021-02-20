1740587058f08e63be7bdb8cd0f075fc0f7887c9
Maro de Melo <marodemelo10@gmail.com>
2021-02-20 11:54:03 -0300

    Criada classe para criptografia md5 da senha de usuário

--- Controllers/Autenticacao.cs
+++ Controllers/Autenticacao.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-
+using Biblioteca.Models;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Biblioteca.Controllers
 {
@@ -8,9 +10,33 @@ namespace Biblioteca.Controllers
     {
         public static void CheckLogin(Controller controller)
         {   
-            if(string.IsNullOrEmpty(controller.HttpContext.Session.GetString("user")))
+            if(string.IsNullOrEmpty(controller.HttpContext.Session.GetString("login")))
             {
                 controller.Request.HttpContext.Response.Redirect("/Home/Login");
+            }
+        }
+
+        public static bool verificaLoginSenha(string login, string senha, Controller controller)
+        {
+            using (BibliotecaContext bc = new BibliotecaContext())
+            {
+                verificaSeUsuarioAdminExiste(bc);
+
+                senha = Criptografo.TextoCriptografado(senha);
+
+                IQueryable<Usuario> UsuarioEncontrado = bc.usuarios.Where(u => u.login == login && u.senha == senha);
+                List<Usuario> ListaUsuarioEncontrado = UsuarioEncontrado.ToList();
+                if(ListaUsuarioEncontrado.Count == 0)
+                {
+                    return false;
+                }
+                else
+                {
+                    controller.HttpContext.Session.SetString("login", ListaUsuarioEncontrado[0].login);
+                    controller.HttpContext.Session.SetString("Nome", ListaUsuarioEncontrado[0].login);
+                    controller.HttpContext.Session.SetString("Tipo", ListaUsuarioEncontrado[0].login);
+                    return true;
+                }
             }
         }
     }
---
+++ Models/Criptografo.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Biblioteca.Models
+{
+    public static class Criptografo
+    {
+        public static string TextoCriptografado(string textoClaro)
+        {
+            MD5 md5Hasher =  MD5.Create();
+            
+
+            byte[] By = Encoding.Default.GetBytes(textoClaro);
+            byte[] bytesCriptografado = md5Hasher.ComputeHash(By);
+
+            StringBuilder SB = new StringBuilder();
+
+            foreach (byte b in bytesCriptografado)
+            {
+                string debugB = b.ToString("x2");
+                SB.Append(debugB);
+            }
+            return SB.ToString();            
+        }
+
+    }
+}
